60a8c5993a37d86b5551d74428346a31d690fd75
chun yu <139964294+Tobapuww@users.noreply.github.com>
2023-12-16 00:33:08 +0800

    Update start.bat

--- device/xiaomi/redmi_note12_turbo/start.bat
+++ device/xiaomi/redmi_note12_turbo/start.bat
@@ -1,12 +1,10 @@
-@echo off 正在等待连接至fastboot
-fastboot getvar product 2>&1 | findstr /r /c:"^product: *marble"
-fastboot getvar product 2>&1 | findstr /r /c:"^product: *marble"
-fastboot flash partition:0 %~dp0images/gpt_both0.bin
-fastboot flash partition:1 %~dp0images/gpt_both1.bin
-fastboot flash partition:2 %~dp0images/gpt_both2.bin
-fastboot flash partition:3 %~dp0images/gpt_both3.bin
-fastboot flash partition:4 %~dp0images/gpt_both4.bin
-fastboot flash partition:5 %~dp0images/gpt_both5.bin
+@echo off 正在修复
+fastboot flash partition:0 gpt_both0.bin
+fastboot flash partition:1 gpt_both1.bin
+fastboot flash partition:2 gpt_both2.bin
+fastboot flash partition:3 gpt_both3.bin
+fastboot flash partition:4 gpt_both4.bin
+fastboot flash partition:5 gpt_both5.bin
 fastboot set_active a
 echo 修复已完成，可按任意键关闭这个窗口
 pause
